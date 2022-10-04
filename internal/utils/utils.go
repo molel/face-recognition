63859cbffafa0d01a8e31778d64162918f178630
molel <kapitov.04@gmail.com>
2022-10-05 01:46:41 +0300

    Create GetEuclideanDistance util

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"errors"
+	"math"
+)
+
+func GetEuclideanDistance(firstPoint []float64, secondPoint []float64) (float64, error) {
+	if len(firstPoint) != len(secondPoint) {
+		return 0, errors.New("points must have the same dimensions")
+	}
+	sum := 0.0
+	for i := 0; i < len(firstPoint); i++ {
+		sum += math.Pow(firstPoint[i]-secondPoint[i], 2)
+	}
+	return math.Sqrt(sum), nil
+}
